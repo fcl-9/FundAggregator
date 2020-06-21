7dbdfe7364a6f33fdc74ac524860cfdd8838810d
Fábio <fabiocl93@gmail.com>
2020-06-21 13:08:18 +0100

    restructure project

---
+++ FundAggregator.API/Controllers/PlatformConfigurationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FundAggregator.API.Commands;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Raven.Client.Documents;
+
+namespace FundAggregator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformConfigurationController : ControllerBase
+    {
+        private readonly IDocumentStore documentDb;
+
+        public PlatformConfigurationController(IDocumentStore documentDb) {
+            this.documentDb = documentDb;
+        }
+
+        [HttpGet]
+        public IActionResult ConfiguredPlatforms() {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost]
+        public IActionResult CreatePlatformConfiguration(CreatePlatformConfiguration platformConfigurationCommand)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(platformConfigurationCommand));
+            return Ok();
+        }
+    }
+}
---
+++ FundAggregator.UI/ClientApp/src/app/models/commands/create-platform-account-configuration-command.ts
@@ -0,0 +1,5 @@
+export class CreatePlatformAccountConfigurationCommand {
+    public platformName: string;
+    public username: string;
+    public password: string;
+}
